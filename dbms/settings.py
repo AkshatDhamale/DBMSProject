5235d9ebb59193416e0cb9ecf152155e85c9a517
akshat dhamale <akshatdhamale@gmail.com>
2021-06-07 15:09:12 +0530

    update4

--- dbms/settings.py
+++ dbms/settings.py
@@ -152,5 +152,4 @@ AUTH_USER_MODEL="crime_management.CustomUser"
 AUTHENTICATION_BACKENDS=['crime_management.EmailBackEnd.EmailBackEnd']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
-prod_db=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500,require_ssl=True))
